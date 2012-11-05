6c4b4541462de8150b9eb2e70ea23035b626a516
Jordan Little <iamjordanlittle@gmail.com>
2012-11-04 20:28:01 -0600

    Major CSS updates and layout changes

--- Yearnly/Yearnly.Web/Scripts/yearnly.js
+++ Yearnly/Yearnly.Web/Scripts/yearnly.js
@@ -1,6 +1,25 @@
 $(function(){
 	
-    $('a[rel]').overlay();
-	
+    $('.tool-select-trigger').click(function (e) {
+        $(this).parents('.header-tools').find('.tool-select').toggle();
+        e.preventDefault();
+    });
+    $('.tool-select a').click(function (e) {
+        activity_type = $(this).attr('class');
+        new_title = $(this).text();
+        toggle_element = $(this).parents('.header-tools').find('.tool-select-trigger');
+        default_title = toggle_element.text();
+
+        toggle_element.text(new_title);
+        
+        if (activity_type == 'all-activity-types') {
+            $('#the_feed li').show();
+        } else {
+            $('#the_feed li').fadeOut(300).delay(300);
+            $('li.' + activity_type + '').fadeIn(300).delay(300);
+        }
+        $(this).parents('.tool-select').toggle();
+        e.preventDefault();
+    });
 	
 });
